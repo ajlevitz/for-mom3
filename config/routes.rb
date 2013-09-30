34cb1476d553c9423396ac87909b39c5affd7959
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-29 20:08:19 -0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ForMom3::Application.routes.draw do
   get "home/index"
+  root 'home#index'
+  resources :users
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
