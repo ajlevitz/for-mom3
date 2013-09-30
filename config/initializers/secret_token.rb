99a4542cbba91a4f620350eb3edc35d490bc2e86
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-29 19:52:46 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForMom3::Application.config.secret_key_base = '139361c7281c415ed75fbeacceb7d3bf035e62132e938bc0f6b08a6fdbc5198e7a2716b5198c9db5113811a7cdfe7177e381299a2f3c671e7809d518c0b889b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ForMom3::Application.config.session_store :cookie_store, key: '_for-mom3_session'
