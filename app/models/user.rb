c2f026043ff4da921590fbaa472214959855cf7f
Alyssa Levitz <alyssal@yammer-inc.com>
2013-10-03 09:24:43 -0700

    validate user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
